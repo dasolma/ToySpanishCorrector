ed00035ac6b0294faaf16d1897f617aec25bd2f0
David <dasolma@gmail.com>
2015-03-16 12:59:01 +0100

    Scrapping from rae and online corrector

--- src/corrector.py
+++ src/corrector.py
@@ -1,6 +1,8 @@
-import re, collections
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re, collections, csv
 
-def words(text): return re.findall('[a-z]+', text.lower()) 
+def words(text): return re.findall('[a-záéíóúñü]+'.decode('utf-8'), text.decode('utf-8').lower())
 
 def train(features):
     model = collections.defaultdict(lambda: 1)
@@ -8,9 +10,46 @@ def train(features):
         model[f] += 1
     return model
 
-NWORDS = train(words(file('quijote.txt').read()))
+def read_dict(files):
+    data = ''
+    for file in files:
+        with open(file, 'r') as f:
+            data += ' ' + f.read()
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return data
+
+
+def read_frecuencies(file='../data/10000_formas.TXT', min = 0):
+    words = {}
+    with open(file, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        next(reader, None)
+        for row in reader:
+            row = [r for r in row if len(r.strip()) > 0]
+            if len(row) > 2 and len(row[0])>0:
+
+                key = row[1].strip().decode('latin-1')
+                frec = int(row[3].replace(".", ""))
+
+                if frec > min:
+                    words[key] = frec
+
+
+    return words
+
+def mix(f1, f2):
+    for f in f2.keys():
+        if f in f1: f1[f] = (f1[f] + f2[f])/2
+        else: f1[f] = f2[f]
+
+    return f1
+
+NWORDS = train( words( read_dict(['../data/news.txt', '../data/conjugations.txt']) ) )
+NWORDS = mix(NWORDS, read_frecuencies(min=2))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzáéíóúñü'.decode('utf-8')
+
+
 
 def edits1(word):
    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
@@ -23,8 +62,57 @@ def edits1(word):
 def known_edits2(word):
     return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
 
+def unknown_edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if not e2 in NWORDS)
+
+
+def known_edits3(word):
+    return set(e2 for e1 in known_edits2(word) for e2 in edits1(e1) if e2 in NWORDS)
+
+def known_edits4(word):
+    return set(e2 for e1 in known_edits3(word) for e2 in edits1(e1) if e2 in NWORDS)
+
 def known(words): return set(w for w in words if w in NWORDS)
 
+def unknown(words): return set(w for w in words if not w in NWORDS)
+
+def candidates(word):
+    word = word.decode('utf-8')
+    kn = known([word])
+    if len(kn) > 0: return kn
+    return (known(edits1(word)).union(known_edits2(word)))
+
 def correct(word):
-    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-    return max(candidates, key=NWORDS.get)
+    cand = candidates(word)
+    if len(cand) == 0: return []
+
+    #take candidates and their frecuencies
+    cand = [{'sug':c, 'frec':NWORDS[c]} for c in cand]
+
+    #calculate accumulated frequency
+    cand = sorted(cand, reverse=True)
+    acu = 0
+    for c in cand:
+        acu += c['frec']
+        c['frec_acu'] = acu
+
+    #return the 80%
+    if( len(cand) > 10):
+        max = acu * 0.5
+        cand = [c for c in cand if c['frec_acu'] < max]
+
+        cand = cand[:10]
+
+
+
+    return cand
+
+
+'''
+Mejoras:
+
+  - errores comunes de teclado
+  - creian -> creían (no crean): los cambios de tilde deben ser mas probables que los de letra
+  - conjugaciones de los verbos. ¿las frecuencias deben ser comunes en todas las conjugaciones de un verbo?
+
+'''
